ac9284c59804f5885c827f49b30c27a4a18b2cc0
Curtis Timson <curtis.timson@whitbread.com>
2017-07-26 14:36:29 +0100

    Add LastFMAPI service and call in directive

--- app/lastfm-nowplaying.js
+++ app/lastfm-nowplaying.js
@@ -2,7 +2,13 @@ angular.module('lastfm-nowplaying', [])
   .directive('lastfmnowplaying', [function(){
 
     var link = function(scope, element, attrs){
-      
+
+      LastFMAPI.getLatestScrobbles(scope.config).then(function(data){
+
+        console.log('lastfmAPI callback');
+
+      });
+
     };
 
     return {
@@ -11,4 +17,27 @@ angular.module('lastfm-nowplaying', [])
       },
       link: link
     },
+  }])
+  .factory('LastFMAPI', ['$q', '$http', function($q, $http){
+
+    var getLatestScrobbles = function(config){
+
+      var apiUrl = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks'
+                    + '&user=' + config.user
+                    + '&api_key=' + config.apiKey
+                    + '&format=json&limit=2';
+
+
+      var defer = $q.defer();
+      $http.get(apiUrl).then(function(data){
+        console.log('data', data);
+        defer.resolve(data);
+      });
+      return defer.promise;
+    };
+
+    return {
+      getLatestScrobbles: getLatestScrobbles
+    };
+
   }]);
