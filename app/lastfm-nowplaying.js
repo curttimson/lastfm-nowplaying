ff7d4d9f03f4fe4b9b34e49c4ae53c74accda245
Curtis Timson <curtis.timson@whitbread.com>
2017-07-26 15:22:34 +0100

    Add imageFx service and call

--- app/lastfm-nowplaying.js
+++ app/lastfm-nowplaying.js
@@ -52,13 +52,15 @@ angular.module('lastfm-nowplaying', [])
     };
 
   }])
-  .factory('canvasUI', [function(){
+  .factory('canvasUI', ['imageFx', function(imageFx){
 
     var applyUI = function(e, canvas, lastFmData){
 
       var imgUrl = lastFmData.data.recenttracks.track[0].image[1]['#text'];
 
-      console.log(imgUrl);
+      imageFx.blur(angular.element(e), canvas, 1, imgUrl, function(){
+        console.log('blur done');
+      });
 
     };
 
@@ -66,4 +68,82 @@ angular.module('lastfm-nowplaying', [])
       applyUI: applyUI
     };
 
-  }]);;
+  }])
+  .factory('imageFx', function ($window, $timeout) {
+
+        var CanvasImage = function (e, t) {
+            this.image = t;
+            this.element = e;
+            this.element.width = this.image.width;
+            this.element.height = this.image.height;
+            this.context = this.element.getContext("2d");
+            this.context.drawImage(this.image, 0, 0);
+        };
+
+        CanvasImage.prototype = {
+            blur: function (e) {
+                this.context.globalAlpha = 0.5;
+                for (var t = -e; t <= e; t += 2) {
+                    for (var n = -e; n <= e; n += 2) {
+                        this.context.drawImage(this.element, n, t);
+                        if (n >= 0 && t >= 0) {
+                            this.context.drawImage(this.element, -(n - 1), -(t - 1));
+                        }
+                    }
+                }
+                this.context.globalAlpha = 1;
+            }
+        };
+
+        var blurGenerator = function (element, canvas, blurAmount, src, callback) {
+
+                var image, canvasImage;
+                var maintainRatio = function() {
+
+                  if((image.width / image.height) > ($window.innerWidth / $window.innerHeight)) {
+
+                    element.height($window.innerHeight);
+                    element.width(element.height() * (image.width / image.height))
+                    element.css('marginLeft', $window.innerWidth - element.width());
+
+                  } else {
+                    element.width($window.innerWidth);
+                    element.height(element.width() * (image.height / image.width));
+                    element.css('marginLeft', 0);
+                  }
+
+                };
+                image = document.createElement("img");
+                image.onload = function () {
+
+                    canvasImage = new CanvasImage(canvas, this);
+                    canvasImage.blur(blurAmount);
+                    //maintainRatio();
+
+                    element.addClass('loaded');
+
+                  if(callback) {
+                      callback();
+                  }
+                };
+                image.src = src;
+
+                angular.element($window).bind('resize', function(){
+                  maintainRatio();
+                });
+
+        };
+
+        return {
+            blur: function (element, blurAmount, src, callback) {
+
+                var canvasSupported = !!$window.HTMLCanvasElement;
+                if(canvasSupported) {
+
+                    return blurGenerator(element, blurAmount, src, callback);
+                } else {
+                    return false;
+                }
+            }
+        };
+    });
