3b503181e2f6fcdd0729baebe92f2abd7bf128b3
Curtis Timson <curt@live.co.uk>
2017-07-26 19:13:54 +0100

    Add stylesheet to example page and include gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,24 @@ var sass = require('gulp-sass');
 
 gulp.task('dev', ['sass-watch']);
 
-gulp.task('sass', function () {
-  return gulp.src('./app/**/*.scss')
+var sassPaths = [
+  './app/**/*.scss',
+  './example/**/*.scss'
+];
+
+gulp.task('sass-app', function () {
+  return gulp.src(sassPaths[0])
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./app'));
 });
 
+gulp.task('sass-example', function () {
+  return gulp.src(sassPaths[1])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./example'));
+});
+
 gulp.task('sass-watch', function(){
-  gulp.watch('./app/**/*.scss', ['sass']);
+  gulp.watch(sassPaths[0], ['sass-app']);
+  gulp.watch(sassPaths[1], ['sass-example']);
 });
