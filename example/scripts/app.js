cedad1f4d0bb52d08af1ba426e98780fbdae3d65
Curtis Timson <curtis.timson@whitbread.com>
2017-07-27 09:42:15 +0100

    Correct example app to 'user'

--- example/scripts/app.js
+++ example/scripts/app.js
@@ -2,6 +2,6 @@ angular.module('app', ['lastfm-nowplaying'])
   .controller('mainCtrl', ['$scope', function($scope){
     $scope.lastFmConfig = {
       apiKey: 'API_KEY',
-      username: 'username'
+      user: 'username'
     };
   }]);
