4de356ce4cd4fbf107d35f704858a480e0dd8e5f
Curtis Timson <curt@live.co.uk>
2017-09-20 21:24:45 +0100

    :heavy_plus_sign: Install PhantomJs

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+            'node_modules/angular/angular.js',
+            'src/**/*.js'
     ],
 
 
@@ -54,7 +56,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
