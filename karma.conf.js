ad03763eeb0f738d469942476deaceae1854a3df
Curtis Timson <curt@live.co.uk>
2017-09-20 21:51:19 +0100

    :heavy_plus_sign: Install angular-mocks and add to karma

--- karma.conf.js
+++ karma.conf.js
@@ -15,9 +15,10 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-            'node_modules/angular/angular.js',
-            'src/**/*.js',
-            'tests/**/*.js'
+        'node_modules/angular/angular.js',
+        'node_modules/angular-mocks/angular-mocks.js',
+        'src/**/*.js',
+        'tests/**/*.js'
     ],
 
 
--- tests/lastfm-nowplaying.test.js
+++ tests/lastfm-nowplaying.test.js
@@ -1,4 +1,17 @@
-describe('lastfm-nowplaying', function () {
+describe('directive: lastfm-nowplaying', function () {
 
+  var element, scope;
+
+  beforeEach(module('lastfm-nowplaying'));
+
+  beforeEach(inject(function($rootScope, $compile) {
+
+    var compiledElement = function(element) {
+      scope = $rootScope.$new();
+      element = $compile(element)(scope);
+      scope.$digest();
+    }
+
+  }));
 
 });
