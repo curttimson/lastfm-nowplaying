c0b7378b4c48d760434b1179f74717c7076c3bfb
Curtis Timson <curt@live.co.uk>
2017-09-20 22:26:02 +0100

    :white_check_mark: Add test for directive with no config

--- tests/lastfm-nowplaying.test.js
+++ tests/lastfm-nowplaying.test.js
@@ -1,17 +1,25 @@
 describe('directive: lastfm-nowplaying', function () {
 
-  var element, scope;
+  var element, scope, rootScope, compile;
 
   beforeEach(module('lastfm-nowplaying'));
 
   beforeEach(inject(function($rootScope, $compile) {
-
-    var compiledElement = function(element) {
-      scope = $rootScope.$new();
-      element = $compile(element)(scope);
-      scope.$digest();
-    }
-
+    rootScope = $rootScope;
+    compile = $compile;
+    scope = rootScope.$new();
   }));
+
+  var compiledElement = function(element) {
+    element = compile(element)(scope);
+
+    scope.$digest();
+    return element;
+  }
+
+  it('should display empty element if no scope data passed', function(){
+    var element = compiledElement('<div lastfmnowplaying></div>');
+    expect(element[0].outerHTML).toBe('<div lastfmnowplaying="" class="ng-scope ng-isolate-scope"></div>');
+  });
 
 });
